4be21eaaae1f5d1f59bb752289f2037e559a0027
Igor <61599472+Mertvbli@users.noreply.github.com>
2020-08-16 17:39:57 +0700

    Add files via upload

---
+++ CW_filter_list_7kyu.py
@@ -0,0 +1,11 @@
+def filter_list(l):
+    new_list = []
+    for number in l:
+        if str(number).isdigit() and str(number) != number:
+            new_list.append(number)
+    return new_list
+
+
+print(filter_list([1,2,'a','b']))
+print(filter_list([1,'a','b',0,15]))
+print(filter_list([1,2,'aasf','1','123',123]))

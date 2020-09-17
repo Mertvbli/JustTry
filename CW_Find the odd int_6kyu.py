45a1bb678985e5fd0a40a24dcdce977cc31987cc
Igor <61599472+Mertvbli@users.noreply.github.com>
2020-09-17 22:06:35 +0700

    Update CW_Find the odd int_6kyu.py

--- CW_Find the odd int_6kyu.py
+++ CW_Find the odd int_6kyu.py
@@ -8,6 +8,12 @@ def find_it(seq):
         if slovar[odd] % 2 != 0:
             return odd
 
+# another option
+def find_it(seq):
+    for i in seq:
+        if seq.count(i)%2!=0:
+            return i
+
 
 print(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
 print(find_it([1,1,2,-2,5,2,4,4,-1,-2,5]))

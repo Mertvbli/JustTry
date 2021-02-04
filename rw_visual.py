bebce0734bc8bc1d2be76182e8457afab11720c3
Igor <61599472+Mertvbli@users.noreply.github.com>
2021-02-04 17:52:23 +0700

    Update rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -2,12 +2,28 @@ import matplotlib.pyplot as plt
 
 from random_walk_CCOP import RandomWalk
 
-# Построение случайного блуждания.
-rw = RandomWalk()
-rw.fill_walk()
+# Новые блуждания строятся до тех пор, пока программа остаётся активной.
+while True:
+    # Построение случайного блуждания.
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
 
-# Нанесение точек на диаграмму.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    # Нанесение точек на диаграмму.
+    plt.style.use('classic')
+    fig, ax = plt.subplots(figsize=(10, 7), dpi=128)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=5)
+
+    # Выделение первой и последней точек.
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Удаление осей.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
